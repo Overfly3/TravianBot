5ee46ec4a1450385f4648f9f979106b2b3984f5e
Overfly3 <a-theijs-moc@hotmail.de>
2015-01-19 14:14:31 +0100

    added browserhandling

--- TravianBot/TravianBot/Form1.cs
+++ TravianBot/TravianBot/Form1.cs
@@ -7,14 +7,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WatiN.Core;
+using WatiN.Core.Interfaces;
+using WatiN.Core.Logging;
+using WatiN.Core.DialogHandlers;
 
 namespace TravianBot
 {
-    public partial class Form1 : Form
+    public partial class Form1 : System.Windows.Forms.Form
     {
         public Form1()
         {
             InitializeComponent();
+
+            test();
         }
+
+        private void test()
+        {
+            SearchForWatiNOnGoogle();
+        }
+
+        public void SearchForWatiNOnGoogle()
+        {
+            using (var browser = new IE("http://www.google.com"))
+            {
+                browser.TextField(Find.ByName("q")).TypeText("WatiN");
+                browser.Button(Find.ByName("btnG")).Click();
+            }
+        } 
     }
 }
